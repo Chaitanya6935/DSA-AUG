79e1bf81f6c8f7c70583f96c32ad4c0a9f8155cd
chaitanya6935 <shindechaitanya847@gmail.com>
2026-08-27 00:14:52 +0530

    Factorial number

--- Mathematical Aptitude_Style Problems/src/Factorial.java
+++ Mathematical Aptitude_Style Problems/src/Factorial.java
@@ -1,6 +1,24 @@
 public class Factorial {
+    public static  int fact(int num){
+
+        if(num==0){
+            return 1;
+        }
+        else{
+
+
+            return num*fact(num-1);
+        }
+
+    }
 
     public static void main(String[] args) {
+        int num = 5;
+        // for(int i = 1;i<=num;i++){
+        //     fact = i*fact;
+        // }
+        int n = fact(num);
+        System.out.println("factorial of "+num+" is : "+n);
 
     }
 }
