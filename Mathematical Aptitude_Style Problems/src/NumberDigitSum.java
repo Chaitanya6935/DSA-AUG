70013339ad705ea425a8f49d41f3e85152501057
chaitanya6935 <shindechaitanya847@gmail.com>
2026-08-31 23:46:04 +0530

    NumberDigitSum

--- Mathematical Aptitude_Style Problems/src/NumberDigitSum.java
+++ Mathematical Aptitude_Style Problems/src/NumberDigitSum.java
@@ -1,7 +1,6 @@
 public class NumberDigitSum {
     public static void main(String[]args){
         int num = 1234,sum=0,degit;
-
         while(num!=0){
             degit = num%10;
             sum+=degit;
